d385b6cb412bb754a5aec207b1e14932fdb8ad00
prudhvikatlaparthi <prudhvikatlaparthi586@gmail.com>
2020-08-11 15:37:19 +0530

    Clock-Update
    Clock-Update

--- lib/model/time.dart
+++ lib/model/time.dart
@@ -1,34 +1,85 @@
+import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class Time {
-  String location; // location name for UI
-  String time; // the time in that location
-  String flag; // url to an asset flag icon
-  String url; // location url for api endpoint
-  bool isDaytime;
-  bool isError;
+class Time extends ChangeNotifier {
+  String _timeT;
+  bool _isLoading = true;
+  bool _isListLoading = false;
+  List _zonesList = [];
+  bool _isDay = true;
+  String _selectedLocation = 'Asia/Kolkata';
+  int _selectedLocationIndex = 197;
+  DateTime _dateTime;
 
-  Time(
-      {this.location,
-      this.time,
-      this.flag,
-      this.url,
-      this.isDaytime,
-      this.isError});
+  Time();
 
-  factory Time.fromJson(Map<String, dynamic> json) {
+  DateTime get dateTime => _dateTime;
+
+  set dateTime(DateTime _dateTime) {
+    this._dateTime = _dateTime;
+  }
+
+  String get time => _timeT;
+
+  set time(String timeT) {
+    this._timeT = timeT;
+    notifyListeners();
+  }
+
+  bool get isLoading => _isLoading;
+
+  set isLoading(bool isLoading) {
+    this._isLoading = isLoading;
+    notifyListeners();
+  }
+
+  bool get isListLoading => _isListLoading;
+
+  set isListLoading(bool isListLoading) {
+    this._isListLoading = isListLoading;
+    notifyListeners();
+  }
+
+  List get zonesList => _zonesList;
+
+  set zonesList(List zonesList) {
+    this._zonesList = zonesList;
+  }
+
+  bool get isDay => _isDay;
+
+  set isDay(bool isDay) {
+    this._isDay = isDay;
+  }
+
+  String get selectedLocation => _selectedLocation;
+
+  set selectedLocation(String selectedLocation) {
+    this._selectedLocation = selectedLocation;
+  }
+
+  int get selectedLocationIndex => _selectedLocationIndex;
+
+  set selectedLocationIndex(int selectedLocationIndex) {
+    this._selectedLocationIndex = selectedLocationIndex;
+  }
+
+  void notify() {
+    notifyListeners();
+  }
+
+  Time fromJson(Map<String, dynamic> json) {
     String datetime = json['datetime'];
     String offset_Hours = json['utc_offset'].substring(1, 3);
     String offset_Minutes = json['utc_offset'].substring(4, 6);
     // create DateTime object
     DateTime now = DateTime.parse(datetime);
-    print('now $now');
-    print('offset H $offset_Hours');
-    print('offset M $offset_Minutes');
-    now = now.add(Duration(hours: int.parse(offset_Hours), minutes: int.parse(offset_Minutes)));
-    return Time(
-        isError: false,
-        time: DateFormat.jm().format(now),
-        isDaytime: now.hour > 6 && now.hour < 18);
+    now = now.add(Duration(
+        hours: int.parse(offset_Hours), minutes: int.parse(offset_Minutes)));
+    Time ti = Time();
+    ti.dateTime = now;
+    ti.time = DateFormat.jm().format(now);
+    ti.isDay = now.hour > 6 && now.hour < 18;
+    return ti;
   }
 }
---
+++ lib/screens/widgets/clock.dart
@@ -0,0 +1,121 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class ClockView extends StatefulWidget {
+  DateTime dateTime;
+  ClockView({@required this.dateTime});
+  @override
+  _ClockViewState createState() => _ClockViewState();
+}
+
+class _ClockViewState extends State<ClockView> {
+  @override
+  void initState() {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    widget.dateTime = widget.dateTime.add(Duration(seconds: 1));
+    return Container(
+      width: 300,
+      height: 300,
+      child: Transform.rotate(
+        angle: -pi / 2,
+        child: CustomPaint(
+          painter: ClockPainter(dateTime: widget.dateTime),
+        ),
+      ),
+    );
+  }
+}
+
+class ClockPainter extends CustomPainter {
+  DateTime dateTime;
+
+  ClockPainter({this.dateTime});
+  //60 sec - 360, 1 sec - 6degree
+  //12 hours  - 360, 1 hour - 30degrees, 1 min - 0.5degrees
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var centerX = size.width / 2;
+    var centerY = size.height / 2;
+    var center = Offset(centerX, centerY);
+    var radius = min(centerX, centerY);
+
+    var fillBrush = Paint()..color = Colors.transparent;
+
+    var outlineBrush = Paint()
+      ..color = Color(0xFFEAECFF)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2;
+
+    var centerFillBrush = Paint()..color = Color(0xFFEAECFF);
+
+    var secHandBrush = Paint()
+      ..color = Colors.orange[300]
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 2;
+
+    var minHandBrush = Paint()
+      ..shader = RadialGradient(colors: [Color(0xFF748EF6), Color(0xFF77DDFF)])
+          .createShader(Rect.fromCircle(center: center, radius: radius))
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 5;
+
+    var hourHandBrush = Paint()
+      ..shader = RadialGradient(colors: [Color(0xFFEA74AB), Color(0xFFC279FB)])
+          .createShader(Rect.fromCircle(center: center, radius: radius))
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 5;
+
+    canvas.drawCircle(center, radius - 40, fillBrush);
+    canvas.drawCircle(center, radius - 40, outlineBrush);
+
+    var hourHandX = centerX +
+        60 * cos((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
+    var hourHandY = centerX +
+        60 * sin((dateTime.hour * 30 + dateTime.minute * 0.5) * pi / 180);
+    canvas.drawLine(center, Offset(hourHandX, hourHandY), hourHandBrush);
+
+    var minHandX = centerX + 80 * cos(dateTime.minute * 6 * pi / 180);
+    var minHandY = centerX + 80 * sin(dateTime.minute * 6 * pi / 180);
+    canvas.drawLine(center, Offset(minHandX, minHandY), minHandBrush);
+
+    var secHandX = centerX + 80 * cos(dateTime.second * 6 * pi / 180);
+    var secHandY = centerX + 80 * sin(dateTime.second * 6 * pi / 180);
+    canvas.drawLine(center, Offset(secHandX, secHandY), secHandBrush);
+
+    canvas.drawCircle(center, 8, centerFillBrush);
+
+    var dashBrush = Paint()
+      ..color = Color(0xFFEAECFF)
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 2;
+    var outerCircleRadius = radius / 1.6;
+    var innerCircleRadius = radius / 1.6 - 15;
+    for (double i = 0; i < 360; i += 30) {
+      var x1 = centerX + outerCircleRadius * cos(i * pi / 180);
+      var y1 = centerX + outerCircleRadius * sin(i * pi / 180);
+
+      var x2 = centerX + innerCircleRadius * cos(i * pi / 180);
+      var y2 = centerX + innerCircleRadius * sin(i * pi / 180);
+      canvas.drawLine(Offset(x1, y1), Offset(x2, y2), dashBrush);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
